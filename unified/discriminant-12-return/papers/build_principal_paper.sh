719f3376b52b5973e53a87b160086b99015b179c
Jeremy Ebert <primeeuler@gmail.com>
2026-09-03 15:36:40 -0400

    Use publication Figure 1a label layout in principal paper build

--- unified/discriminant-12-return/papers/build_principal_paper.sh
+++ unified/discriminant-12-return/papers/build_principal_paper.sh
@@ -8,7 +8,7 @@ PAPER="Discriminant_12_Return_v0.3.3.tex"
 
 printf '==> generating publication figures\n'
 python3 "$FIG/make_mod12_v4_cone_triple.py"
-python3 "$FIG/fig_cutting_plane_tangent_circle_audit.py"
+python3 "$FIG/fig_cutting_plane_tangent_circle_publication.py"
 python3 "$FIG/fig_divisor_summatory_11_3panel.py"
 
 printf '==> compiling %s\n' "$PAPER"
