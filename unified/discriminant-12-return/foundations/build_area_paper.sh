a89eb4a6fdec133c09a9b6ef5d6f4692fcee6e6c
Jeremy Ebert <primeeuler@gmail.com>
2026-09-04 11:07:50 -0400

    Use Paper A named area figure builder

--- unified/discriminant-12-return/foundations/build_area_paper.sh
+++ unified/discriminant-12-return/foundations/build_area_paper.sh
@@ -5,8 +5,8 @@ HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FIG="$HERE/../figures"
 PAPER="Note_AreaDistortion_AMGM_Cone_v1.1.tex"
 
-printf '==> generating area-paper figure\n'
-python3 "$FIG/fig_area_measure_11_2panel.py"
+printf '==> generating Paper A area-companion figure\n'
+python3 "$FIG/paperA_area_measure_11_2panel.py"
 
 printf '==> compiling %s in foundations/\n' "$PAPER"
 cd "$HERE"
